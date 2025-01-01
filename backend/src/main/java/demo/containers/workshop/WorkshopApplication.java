1eb4170fe677989224c26f96e631d3b63f924412
lucero garcia <ml17ft@gmail.com>
2025-01-01 16:27:41 -0600

    Add backend excercise app

---
+++ backend/src/main/java/demo/containers/workshop/WorkshopApplication.java
@@ -0,0 +1,18 @@
+package demo.containers.workshop;
+
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopApplication.class, args);
+	}
+}
